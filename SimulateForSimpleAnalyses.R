9cfe040fd297a015979c58c0ab62b273decc05a1
Ryo Nakagawara <ryonakagawara@gmail.com>
2017-04-01 14:51:24 +0900

    - create simulated dataset

--- SimulateForSimpleAnalyses.R
+++ SimulateForSimpleAnalyses.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 # Simulating data for simpel data analyses
 # Dataset: total sales by week for 2 products at chain stores (T = 2 years)
 k.stores <- 20
@@ -31,3 +33,47 @@ rm(store.num)
 (store.df$Year <- rep(rep(1:2, each = k.weeks/2), times = k.stores))
 str(store.df)
 
+# Change variable type for $country and $storeNum to factor: 
+# (store.Num is a lable, NOT integer/number)
+store.df$storeNum <- store.df$storeNum %>% factor()
+store.df$country <- store.df$country %>% factor()
+# Check changes
+store.df %>% str()
+store.df %>% head()
+store.df %>% head(120)
+store.df %>% tail()
+store.df %>% tail(120)
+
+# Input sales, price, prom data with PRNG, set.seed(): 
+set.seed(98250)
+# Each row: one week of one year for one store
+?rbinom
+store.df$p1prom <- rbinom(n = nrow(store.df), size = 1, p = 0.1)  # 10$ chance of promotion
+store.df$p2prom <- rbinom(n = nrow(store.df), size = 1, p = 0.15) # 15% chance of promotion
+head(store.df)
+
+# Each product 5 distinct prices, price range from $2.19 ~ $3.19, use sample():
+store.df$p1price <- sample(x = c(2.19, 2.29, 2.49, 2.79, 2.99), size = nrow(store.df), replace = TRUE)
+store.df$p2price <- sample(x = c(2.29, 2.49, 2.59, 2.99, 3.19), size = nrow(store.df), replace = TRUE)
+
+# Simulate sales figures for each week, use poisson distribution.
+# Sales as function of relative prices of P1 and P2 with their promotional status
+# Default sales in absence of promotion:
+tmp.sales1 <- rpois(nrow(store.df), lambda = 120)
+tmp.sales2 <- rpois(nrow(store.df), lambda = 100)
+
+# Scale unit count of item sales depending on relative prices, log function:
+tmp.sales1 <- tmp.sales1 * log(store.df$p2price) / log(store.df$p1price)
+tmp.sales2 <- tmp.sales2 * log(store.df$p1price) / log(store.df$p2price)
+# ASSUMPTION: sales variance due to inverse ratio of prices
+# i.e. sales of P1 go up to degree log(price) of P1 is LOWER than log(price) of P2.
+
+# ASSUMPTION: sales = +30% (P1) and +40% (P2) when each product is promoted. 
+# Promotional status vector (0 or 1) * 0.3 or 0.4
+?floor()
+store.df$p1sales <- floor(tmp.sales1 * (1 + store.df$p1prom * 0.3))
+store.df$p2sales <- floor(tmp.sales2 * (1 + store.df$p2prom * 0.4))
+
+head(store.df)
+library(car)
+some(store.df)
